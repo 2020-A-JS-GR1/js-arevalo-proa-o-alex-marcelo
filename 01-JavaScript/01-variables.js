2fa1262314c28cb5746f1bb243e64513ca2fca50
Alex Arevalo <arevaloalex9@gmail.com>
2020-06-12 17:58:02 -0500

    objetos JSON

--- 01-JavaScript/01-variables.js
+++ 01-JavaScript/01-variables.js
@@ -19,6 +19,7 @@ const texto = "Alex"; // string
 const booleanoo = false; // boolean
 const hijos = null; // object
 const zapatos = undefined; //undefined
+const apellido = 'Arevalo';
 
 console.log(typeof numero);
 console.log(typeof sueldo);
@@ -26,3 +27,77 @@ console.log(typeof texto);
 console.log(typeof booleanoo);
 console.log(typeof hijos);
 console.log(typeof zapatos);
+console.log(typeof apellido);
+
+if (true == true) {
+    console.log('Es Truty'); // Truty
+} else {
+    console.log('Es Falsy');
+}
+
+if ("Alex") {
+    console.log('Es Truty'); // Truty
+} else {
+    console.log('Es Falsy');
+}
+
+if (-1) {
+    console.log('Es Truty'); // Truty
+} else {
+    console.log('Es Falsy');
+}
+
+if (0) {
+    console.log('Es Truty');
+} else {
+    console.log('Es Falsy'); // Falsy
+}
+
+if (1) {
+    console.log('Es Truty'); // Truty
+} else {
+    console.log('Es Falsy');
+}
+
+if (null) {
+    console.log('Es Truty');
+} else {
+    console.log('Es Falsy'); // Falsy
+}
+
+if (undefined) {
+    console.log('Es Truty');
+} else {
+    console.log('Es Falsy'); // Falsy
+}
+
+// Objectos Js (JSON) - Arreglos
+
+const alex = {
+    nombre: "Alex", // llave: valor
+    'apellido': 'Arevalo',
+    edad: 31,
+    hijos: null,
+    zapatos: undefined,
+    ropa: {
+        color: 'azul',
+        talla: 40
+    },
+    mascotas: ['gato','perro','paloma']
+}; // object
+
+// Acceder a los objecto
+alex.nombre; // "Alex"
+alex.apellido; // "Alex"
+
+const arregloNumeros = []; // object
+
+console.log(alex);
+console.log(arregloNumeros);
+
+
+
+
+
+
+
