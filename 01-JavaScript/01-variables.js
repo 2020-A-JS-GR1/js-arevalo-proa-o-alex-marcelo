9449d07814d6491350f51aa975de2bdad26eaef7
Alex Arevalo <arevaloalex9@gmail.com>
2020-06-12 17:23:39 -0500

    tipos de variables

---
+++ 01-JavaScript/01-variables.js
@@ -0,0 +1,28 @@
+// Variables Mutables e Inmutables
+
+// Variables Mutables
+var numeroUno = 1;
+let numeroDos = 2;
+numeroUno = 5;
+numeroDos = 8;
+numeroUno = false;
+numeroDos = true;
+
+// Variables Inmutables
+const configuracionArchivos = "PDF";
+// configuracionArchivos = "XML";
+
+// Tipos de Variables
+const numero = 1; // number
+const sueldo = 1.2; // number
+const texto = "Alex"; // string
+const booleanoo = false; // boolean
+const hijos = null; // object
+const zapatos = undefined; //undefined
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof booleanoo);
+console.log(typeof hijos);
+console.log(typeof zapatos);
