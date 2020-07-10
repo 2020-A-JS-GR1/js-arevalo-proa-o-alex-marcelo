b425ad433624fcb887ea1e0dfdd62fbe3d1ce96c
Alex <arevaloalex9@gmail.com>
2020-07-10 17:46:33 -0500

    TypeScript
    variables e interfaces

---
+++ 02-typescript/01-variables.ts
@@ -0,0 +1,19 @@
+let nombre: string = 'Alex';
+// nombre = 1;
+nombre = 'Marcelo';
+let edad: number = 31;
+let casado: boolean = true;
+let fecha: Date = new Date();
+
+
+let marihuana: any = 2;
+marihuana = '2';
+marihuana = true;
+marihuana = () => 2;
+
+let edadMultiple: number | string = 2;
+edadMultiple = '2';
+edadMultiple = 25;
+edadMultiple = 'dos';
+
+
